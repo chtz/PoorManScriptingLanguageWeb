c15f5b720d32e4db3045c015e8e51b012db28e56
Christian Tschenett <chtz@me.com>
2017-01-31 14:30:15 +0100

    initial

---
+++ src/main/java/ch/furthermore/pmslweb/ScriptController.java
@@ -0,0 +1,52 @@
+package ch.furthermore.pmslweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.script.ScriptException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ch.furthermore.pmsl.ScriptFunction;
+
+/**
+ * Samples
+ * </pre>
+ * curl -s -d 'def foo() ret "hallo" end' -H"Content-Type:text/plain" http://localhost:8080/ 
+ * </pre>
+ */
+@Controller
+@EnableAutoConfiguration
+public class ScriptController {
+	@RequestMapping("/")
+	@ResponseBody
+	String home() throws NoSuchMethodException, IOException, ScriptException {
+		String sample = "curl -s -d 'def foo() ret \"hallo\" end' -H'Content-Type:text/plain' http://<host>:<post>/".replaceAll("\"", "\\\\\"");
+		return executeScript("def hello() ret \"" + sample + "\" end");
+	}
+	
+	@RequestMapping(path="/",consumes="text/plain",produces="text/plain",method=RequestMethod.POST)
+	@ResponseBody
+	String executeScript(@RequestBody String script) {
+		try {
+			ScriptFunction f = new ScriptFunction(script);
+			return f.invoke().toString();
+		}
+		catch (Exception e) {
+			StringWriter sw = new StringWriter();
+			e.printStackTrace(new PrintWriter(sw));
+			return sw.toString();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ScriptController.class, args);
+	}
+}
