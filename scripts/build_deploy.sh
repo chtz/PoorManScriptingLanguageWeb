7d083bcb9513694234eb30b45be2b26a96355694
Christian Tschenett <chtz@me.com>
2017-02-14 21:19:15 +0100

    deploy script adjusted

--- scripts/build_deploy.sh
+++ scripts/build_deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mvn package docker:build
 docker push dockerchtz/pmsl-web:latest
-ssh root@pmsl 'docker rm -f $(docker ps -a -q) && docker rmi dockerchtz/pmsl-web && docker run -p 8080:8080 -d dockerchtz/pmsl-web'
-
+ssh root@pmsl '(docker rm -f pmsl-web; true) && docker rmi dockerchtz/pmsl-web && docker run -p 8080:8080 --name pmsl-web -d dockerchtz/pmsl-web'
